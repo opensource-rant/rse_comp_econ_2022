ab5da7087f5e2481b002f1fb83bb60ec15b428fb
Richard Anthony <public.rant@pm.me>
2022-07-18 13:25:18 +1000

    fixup! (chore): QuantEcon Workshop

--- countdown_test.py
+++ countdown_test.py
@@ -5,5 +5,9 @@ class TestCoreFunctions(unittest.TestCase):
     def test_reverse_range(self):
         self.assertEqual(list(reversed(range(0,11))), [10,9,8,7,6,5,4,3,2,1,0])
 
+    def test_count_char(self):
+        s = "foobarfoobarfoobarfoobarfoobarfoobar"
+        self.assertEqual(len(s.split(" ")), 8)
+
 if __name__ == '__main__':
     unittest.main()
